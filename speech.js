efe6b435b5a9f7e35d01880b67570214782f4b47
Pieter de Vries <pieterandcamryn@gmail.com>
2021-08-24 14:58:01 -0700

    Add speach recognition

---
+++ speech.js
@@ -0,0 +1,147 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let text = "";
+
+recognition.addEventListener("result", (e) =>
+{
+	text = Array.from(e.results)
+	                  .map((result) => result[0])
+	                  .map((result) => result.transcript)
+	                  .join("");
+	
+	if (e.results[0].isFinal)
+	{
+		console.log(text);
+		
+		if (text.includes("you") || text.includes("elon") || text.includes("Elon") || text.includes("elan") || text.includes("Elan") || text.includes("alon") || text.includes("Alon"))
+		{
+			if (text.includes("time"))
+			{
+				$("#message").val("The time is " + $('#time').html());
+				
+				text = "";
+			}
+			else if (text.includes("date"))
+			{
+				$("#message").val("The date is " + $('#date').html());
+				
+				text = "";
+			}
+			else if (text.includes("temperature"))
+			{
+				$("#message").val("The temperature is " + $('#temperature').html() + " Fahrenheit");
+				
+				text = "";
+			}
+			else if (text.includes("weather"))
+			{
+				$("#message").val("The temperature is " + $('#temperature').html() + " Fahrenheit");
+				
+				text = "";
+			}
+			else if (text.includes("wind") && text.includes("speed"))
+			{
+				$("#message").val("The wind speed is " + $('.windspeed').html());
+				
+				text = "";
+			}
+			else if (text.includes("pressure"))
+			{
+				$("#message").val("The air pressure is " + $('.pressure').html().replaceAll(" hPa", "") + " pascal");
+				
+				text = "";
+			}
+			else if (text.includes("humidity"))
+			{
+				$("#message").val("The humidity is " + $('.humidity').html());
+				
+				text = "";
+			}
+			else if (text.includes("aqi") || text.includes("air") || text.includes("quality"))
+			{
+				$.getJSON("https://api.waqi.info/feed/grants%20pass/?token=14a9d611a3e0a2ca8e55a44d73fa5541970a3020", function(json)
+				{
+					let aqi = JSON.stringify(json.data.aqi);
+					$("#message").val("The air quality is " + getAQIText(aqi) + " at " + aqi);
+				});
+				
+				text = "";
+			}
+			else
+			{
+				$("#message").val("");
+				text = "";
+			}
+		}
+	}
+});
+
+let speech;
+
+recognition.addEventListener("end", () =>
+{
+	speech.text = $("#message").val();
+	speech.volume = 1;
+	speech.rate = 1;
+	speech.pitch = 1;
+	
+	window.speechSynthesis.speak(speech);
+	
+	recognition.start();
+	
+	text = "";
+	$("#message").val("");
+});
+
+$("#speakButton").on("click", function ()
+{
+	speech = new SpeechSynthesisUtterance();
+	$("#speakButton").hide();
+	
+	speech.addEventListener('start', function(event)
+	{
+		recognition.stop();
+	});
+	
+	speech.addEventListener('end', function(event)
+	{
+		recognition.start();
+	});
+});
+
+recognition.start();
+
+function getAQIText(aqi)
+{
+	let name = "";
+	
+	if (aqi <= 50)
+	{
+		name = "Good";
+	}
+	else if (aqi > 50 && aqi <= 100)
+	{
+		name = "Moderate";
+	}
+	else if (aqi > 100 && aqi <= 150)
+	{
+		name = "Unhealthy for sensitive groups";
+	}
+	else if (aqi > 150 && aqi <= 200)
+	{
+		name = "Unhealthy";
+	}
+	else if (aqi > 200 && aqi <= 500)
+	{
+		name = "Very Unhealthy";
+	}
+	else if (aqi > 500)
+	{
+		name = "Hazardous";
+	}
+	
+	return name;
+}
